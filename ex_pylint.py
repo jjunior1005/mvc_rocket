01af5adca850dd7140a3dcb0bfa1b4ed5817ed25
Joao Junior <jjunior1005@gmail.com>
2024-11-27 06:33:08 -0300

    config: Addind Pylint to project

---
+++ ex_pylint.py
@@ -0,0 +1,9 @@
+
+print("Olá Mundo")
+
+def minha_funcao() -> None:
+    print("Minha funcao")
+
+# snake_case -> variáveis e funções em python (ex. my_variable)
+# cammelcase -> Não consta: minhaFuncao xxxxxx
+# PascalCase -> Classe em Python
